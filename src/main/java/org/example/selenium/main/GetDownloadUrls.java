4997276c53b73cea52f8413efbbd7e8b0b498898
xiaobailong <gaoqingyue08@gmail.com>
2025-03-23 08:53:30 +0800

    增加列表保存功能

---
+++ src/main/java/org/example/selenium/main/GetDownloadUrls.java
@@ -0,0 +1,40 @@
+package org.example.selenium.main;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.io.FileUtil;
+import org.example.selenium.db.PageHistoryTable;
+import org.example.selenium.enums.DateTimeFormatEnum;
+import org.example.selenium.enums.FileEnums;
+
+import java.util.List;
+
+public class GetDownloadUrls {
+
+    public static void main(String[] args) {
+//        getDownloadUrls();
+        saveDownloadUrls();
+    }
+
+    public static void getDownloadUrls() {
+        String listFileName = FileEnums.SAVE_PATH + "/" + new DateTime().toString(DateTimeFormatEnum.PATH_DATE) + ".txt";
+        List<String> lines = FileUtil.readUtf8Lines(listFileName);
+        for (String line : lines) {
+            if (PageHistoryTable.isExist(line)) {
+                continue;
+            }
+            System.out.println(line);
+        }
+    }
+
+    public static void saveDownloadUrls() {
+        String listFileName = FileUtil.getUserHomePath() + "/Downloads/save.txt";
+        List<String> lines = FileUtil.readUtf8Lines(listFileName);
+        for (String line : lines) {
+            if (PageHistoryTable.isExist(line)) {
+                continue;
+            }
+            PageHistoryTable.insert(line, "");
+            System.out.println(line);
+        }
+    }
+}
